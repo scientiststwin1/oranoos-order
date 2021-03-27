854beaf71ae0a2321a922e5f4d6debf0f2af4ebb
Ali Abbasi <Ali@localhost.localdomain>
2021-03-27 16:58:04 +0430

    feature: add auth and admin-control to project

--- src/order/order.controller.ts
+++ src/order/order.controller.ts
@@ -1,6 +1,10 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import { Body, Controller, Get, Post, UseGuards } from '@nestjs/common';
+import { ApiBearerAuth } from '@nestjs/swagger';
+import { AdminGuard } from 'src/shared/auth/admin-control.guard';
+import { JwtAuthGuard } from 'src/shared/auth/jwt.guard';
 import { RegisterOrderDto } from './model/order.dto';
 import { OrderService } from './order.service';
+import { UserRole } from "../shared/user.enum";
 
 @Controller('order')
 export class OrderController {
--- src/shared/auth/admin-control.guard.ts
+++ src/shared/auth/admin-control.guard.ts
@@ -1,10 +1,11 @@
 import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common'
 import { Reflector } from '@nestjs/core'
 import { ResultError } from '../main.helper'
+import { UserRole } from '../user.enum'
 
 
 @Injectable()
-export class ACGuard implements CanActivate {
+export class AdminGuard implements CanActivate {
   constructor(private readonly reflector: Reflector) {}
 
   protected async getUser(context: ExecutionContext): Promise<any> {
@@ -12,31 +13,18 @@ export class ACGuard implements CanActivate {
     return request.user
   }
 
-  protected async getUserRoles(context: ExecutionContext): Promise<string | string[]> {
+  protected async getUserRoles(context: ExecutionContext): Promise<UserRole> {
     const user: any = await this.getUser(context)
     if (!user) throw new ResultError(null, 403, 403, 'Unauthorized')
-
-    if (!user.roles) throw new ResultError(null, 403, 403, 'Denied')
-
-    return user.roles
+    return user.role
   }
 
   public async canActivate(context: ExecutionContext): Promise<boolean> {
 
-    let roles = this.reflector.get<string[]>('roles', context.getHandler())
-
-    const args = context.getArgByIndex(0)
-
-    if (args.roles) roles = args.roles
-
     const userRoles = await this.getUserRoles(context)
 
-    if (roles) {
-      for (const role in roles) {
-        if (userRoles?.includes(roles[role])) {
-          return true
-        }
-      }
+    if(userRoles == UserRole.ADMIN){
+        return true
     }
 
     throw new ResultError(null, 403, 403, 'Denied')
